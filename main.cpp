bc72b1517e4634472ac45e1b74b036f57a6492aa
coonn <coonne@qq.com>
2019-05-25 16:24:09 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    // '<<' 表示吧Hello, World!插入到cout里去打印，'endl'是控制符，表示重起一行。
+    // 'endl'与'\n'区别，'\n'只做换行工作，'endl'除了换行还有确保清流的动作（所有缓存都打印）
+    return 0;
+}
